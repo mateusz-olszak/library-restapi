e1a8969bedb1f9e377b7cf52dd752d9fd5bfd5a7
mateusz-olszak <mateuszolszak@gmail.com>
2022-01-19 01:56:08 +0100

    Add scheduler logs

--- src/main/java/com/library/scheduler/EmailScheduler.java
+++ src/main/java/com/library/scheduler/EmailScheduler.java
@@ -4,11 +4,13 @@ import com.library.config.AdminConfig;
 import com.library.domain.Mail;
 import com.library.service.SimpleEmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EmailScheduler {
 
     private static final String SUBJECT = "Library: Books info";
@@ -18,12 +20,14 @@ public class EmailScheduler {
 
     @Scheduled(cron = "0 0 12 * * MON")
     public void sendInformationEmail() {
+        log.info("preparing to send weekly database summary email");
         emailService.send(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT,
                 "Weekly summary of books status in the database.",
                 null
         ));
+        log.info("email has been sent");
     }
 
 }
